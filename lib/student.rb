ecb7a749fa0386d742be8a7f8c79342ec734ddc0
github username <github email address>
2017-11-04 18:45:49 -0400

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,14 @@
+class Student
+  def initialize
+    hello
+    raise_hand
+  end
+
+  def hello
+    puts "Hey there! I'm so excited to learn stuff.\n"
+  end
+
+  def raise_hand
+    puts "Pick me!\n"
+  end
+end
